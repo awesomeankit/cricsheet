b0252bda5f0089da96a0b086cae5f0d458444106
awesomeankit <ankitsawant40@gmail.com>
2021-09-10 13:07:42 +0530

    First commit with basic project structure

---
+++ src/main/java/com/cricket/cricsheet/CricsheetApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.cricsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricsheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket/cricsheet/CricsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.cricsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricsheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
